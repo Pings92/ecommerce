e06e9e3581544126034c104d6428bfc681643aea
pierickj <pierick.jacquet@gmail>
2026-02-24 14:52:11 +0100

    maj 240220261451 site ecommerce

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function index(): Response
+    {
+        $namesStudents = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
+        $age = 20;
+        
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+            'names_students' => $namesStudents,
+            'age' => $age
+        ]);
+    }
+}
