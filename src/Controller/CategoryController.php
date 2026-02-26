022975d85a0a2be861f6efcf6ee1410689783b83
pierickj <pierick.jacquet@gmail>
2026-02-26 14:56:40 +0100

    Setting up navbar

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'app_category')]
+    #[Route('/admin/category', name: 'app_category')]
     public function index(CategoryRepository $repo): Response // La méthode index reçoit une instance de CategoryRepository en tant que paramètre,
     // ce qui lui permet d'accéder aux méthodes de ce repository pour interagir avec la base de données.
     // La méthode index est responsable de récupérer toutes les catégories de la base de données et de les passer à la vue Twig pour affichage.
@@ -28,7 +28,7 @@ final class CategoryController extends AbstractController
             // $categories est la variable qui contient les catégories récupérées de la base de données via le repository
             ]);
     }
-    #[Route('/category/new', name: 'app_category_new')]
+    #[Route('/admin/category/new', name: 'app_category_new')]
     public function addCategory(EntityManagerInterface $taxi, Request $request): Response
     {
         $category = new Category();
@@ -39,13 +39,16 @@ final class CategoryController extends AbstractController
         if ($formulaire->isSubmitted() && $formulaire->isValid()) {
             $taxi->persist($category);
             $taxi->flush();
+            $this->addFlash('success', 'Catégorie ajoutée avec succès');
+            return $this->redirectToRoute('app_category');
             }
+        
         return $this->render('category/newCategory.html.twig', [
             'form' => $formulaire->createView(),
         ]);
     }
 
-    #[Route('/category/update/{id}', name: 'app_category_update')]
+    #[Route('/admin/category/update/{id}', name: 'app_category_update')]
         public function updateCategory (Category $category, EntityManagerInterface $entityManagerInterface, Request $request): Response
     {
         $form = $this-> createForm(CategoryFormType::class, $category);
@@ -53,14 +56,14 @@ final class CategoryController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()){
             $entityManagerInterface->flush();
-
+            $this->addFlash('success', 'Catégorie modifiée avec succès');
             return $this->redirectToRoute('app_category');
         }
         return $this->render('category/updateCategory.html.twig',[
         'form' => $form->createView(),
     ]);
     }
-    #[Route('/category/delete/{id}', name: 'app_category_delete')]
+    #[Route('/admin/category/delete/{id}', name: 'app_category_delete')]
     public function deleteCategory(Category $categoryASuprimer, EntityManagerInterface $entityManagerInterface): Response
     {
          // La méthode deleteCategory est actuellement vide, ce qui signifie qu'elle n'a pas encore été implémentée pour gérer la suppression d'une catégorie.
@@ -68,7 +71,10 @@ final class CategoryController extends AbstractController
          // puis utiliser ces outils pour trouver la catégorie à supprimer et effectuer la suppression de la base de données.
     $entityManagerInterface-> remove($categoryASuprimer);
     $entityManagerInterface-> flush();
-
+    $this->addFlash('danger', 'Catégorie supprimée avec succès');// Ajoute un message flash pour indiquer que la catégorie a été supprimée avec succès
+    //success est le type de message flash, et 'Catégorie supprimée avec succès' est le contenu du message qui sera affiché à l'utilisateur après la suppression de la catégorie.
+    //success est un type de message flash qui indique que l'opération a été effectuée avec succès. Lorsque vous ajoutez un message flash de type success, il est généralement utilisé pour informer l'utilisateur que l'action qu'il a entreprise a été réalisée avec succès, comme la création, la mise à jour ou la suppression d'une ressource.
+    //success correspond à une classe CSS qui peut être utilisée pour styliser le message flash de manière appropriée, souvent avec une couleur verte ou un indicateur visuel positif pour signaler le succès de l'opération.
     return $this->redirectToRoute('app_category');
 
     }
