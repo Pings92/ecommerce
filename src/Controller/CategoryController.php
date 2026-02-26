c2abb569bb6effd10b7b1c90d0ee4b31bd265a8a
pierickj <pierick.jacquet@gmail>
2026-02-26 10:12:18 +0100

    Update Category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -20,19 +20,35 @@ final class CategoryController extends AbstractController
         ]);
     }
     #[Route('/category/new', name: 'app_category_new')]
-    public function addCategory(EntityManagerInterface $entityManagerInterface, Request $request): Response
+    public function addCategory(EntityManagerInterface $taxi, Request $request): Response
     {
         $category = new Category();
 
-        $form = $this->createForm(CategoryFormType::class, $category);
-        $form->handleRequest($request);
+        $formulaire = $this->createForm(CategoryFormType::class, $category);
+        $formulaire->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManagerInterface->persist($category);
-            $entityManagerInterface->flush();
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $taxi->persist($category);
+            $taxi->flush();
             }
         return $this->render('category/newCategory.html.twig', [
-            'form' => $form->createView(),
+            'form' => $formulaire->createView(),
         ]);
+    }
+
+    #[Route('/category/update/{id}', name: 'app_category_update')]
+        public function updateCategory (Category $category, EntityManagerInterface $entityManagerInterface, Request $request): Response
+    {
+        $form = $this-> createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('app_category');
+        }
+    return $this->render('category/updateCategory.html.twig',[
+        'form' => $form->createView(),
+    ]);
     }
 }
