5fd191f976c773ce626a38336c2d7897faf54aab
pierickj <pierick.jacquet@gmail>
2026-02-27 10:29:12 +0100

    setting up delete and edit user/role

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    #[Route('/users', name: 'app_users')]
+    #[Route('/users', name: 'app_user')]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
-            $users = $userRepository->findAll(),
-            'controller_name' => 'UserController',
+            'users' => $userRepository->findAll(),
+            // 'controller_name' => 'UserController',
         ]);
     }
+
+    #[Route('/admin/users/editRole/{id}', name: 'app_user_edit_role')]
+    public function editRole(User $user, EntityManagerInterface $entityManagerInterface, UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+
+        ]);
+    }
+
+    // #[Route('/admin/users/delete/{id}', name: 'app_user_delete')]
+    // public function deleteUser(EntitymanagerInterface $entityManagerInterface, UserRepository $userRepository): Response
+    // {
+    //     return $this->render('user/index.html.twig', []);
+    // }
 }
