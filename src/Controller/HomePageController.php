13622e5c96a20473cd31ca1a02f042b2fc5cdb5d
pierickj <pierick.jacquet@gmail>
2026-03-13 11:45:48 +0100

    nav bar category

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\DisplayProductType;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use App\Repository\SubCategoryRepository;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,25 +15,28 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, SubCategoryRepository $subCategoryRepository): Response
     {
         $products = $productRepository->findAll();
         return $this->render('home_page/index.html.twig', [
             // 'controller_name' => 'HomePageController',
-            'products'=> $products
+            'products'=> $products,
+            'categories'=>$categoryRepository->findAll(),
+            'subCategories'=>$subCategoryRepository->findAll()
+
         ]);
     }
 
     #[Route('product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
-    public function showProduct(Product $product, ProductRepository $productRepository): Response
+    public function showProduct(Product $product, ProductRepository $productRepository, CategoryRepository $categoryRepository, SubCategoryRepository $subCategoryRepository): Response
     {return $this->render('home_page/show.html.twig', [
         'product'=>$product,
-        'lastProductAdded'=> $productRepository->findBy([], ['id'=>'DESC'], 3)
+        'lastProductAdded'=> $productRepository->findBy([], ['id'=>'DESC'], 3),
+        'categories'=>$categoryRepository->findAll(),
+        // 'subCategories'=>$subCategoryRepository->findBy()
     ]);
     }
 
-
-    
     // #[Route('/display', name: 'app_product_on_home_page')]
     // public function showProductHomepage($id, ProductRepository $productRepository): Response
     // {
