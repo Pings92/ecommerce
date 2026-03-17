a3f6efa07346f1217ab36c5b05114242bb2e3059
pierickj <pierick.jacquet@gmail>
2026-03-17 15:12:17 +0100

    order (cart)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Order;
+use App\Form\OrderType;
 
 final class OrderController extends AbstractController
 {
     #[Route('/order', name: 'app_order')]
     public function index(): Response
     {
+        $order = New Order();
+        $form = $this->createform(OrderType::class, $order);
         return $this->render('order/index.html.twig', [
             'controller_name' => 'OrderController',
         ]);
